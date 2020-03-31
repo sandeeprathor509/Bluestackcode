64d1ff0ff1f9c5eb3bb1784b636fca2ff27e6cc4
sandeeprathor509 <sandeeprathor509@gmail.com>
2020-03-31 22:48:01 +0530

    Update Bluestack.py

--- Bluestack.py
+++ Bluestack.py
@@ -1,15 +1,19 @@
 def reverse_each_word(sentence):
+    """
+    This will only change/replaces position of
+    the alpha char not the special char
+    """
     temp_var = ""
     reverse_str = ""
     for char in sentence:
 
         if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
-            temp_var = char + temp_var
+            temp_var = char + temp_var  # temp_var will contain the special char
 
         else:
             reverse_str = reverse_str + temp_var + char
             temp_var = ""
-            reverse_str = reverse_str + temp_var
+            reverse_str = reverse_str + temp_var  # reverse_str will contain all char including special
 
     return reverse_str
 
